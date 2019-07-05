69003393417e9640f985ee7ab5359994fe9951e2
Japod_laptop <Jvivas97@gmail.com>
2019-07-05 14:22:33 -0400

    trabajos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,58 +16,7 @@ export class AppComponent {
 
 
 
-  public changeAds(val: any) {
-    switch (val) {
-      case 0:
-        this.changeTab = 0;
-        break;
-      case 1:
-        this.changeTab = 1;
-        break;
-      case 2:
-        this.changeTab = 2;
-        break;
-      case 3:
-        this.changeTab = 3;
-        break;
-      case 4:
-        this.changeTab = 4;
-        setInterval(() => {
-          this.changeCarrousel();
-        }, 8000);
 
-        break;
-
-      default:
-        break;
-    }
-  }
-
-  changeManualCarrousel(val: any) {
-// tslint:disable-next-line: triple-equals
-    if (val == 1) {
-      if (this.carrousel_img === 3) {
-        this.carrousel_img = 0;
-      } else {
-        this.carrousel_img = this.carrousel_img + 1;
-      }
-// tslint:disable-next-line: one-line
-    }else{
-      if (this.carrousel_img === 0) {
-        this.carrousel_img = 3;
-      } else {
-        this.carrousel_img = this.carrousel_img - 1;
-      }
-    }
-  }
-
-  changeCarrousel() {
-    if (this.carrousel_img == 3) {
-      this.carrousel_img = 0
-    } else {
-      this.carrousel_img = this.carrousel_img + 1;
-    }
-  }
 }
 
 
