6605938f428642354de79269ab3da97121b42ca3
Japod_laptop <Jvivas97@gmail.com>
2019-07-05 20:20:44 -0400

    Cambios varios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,28 @@ export class AppComponent {
 // tslint:disable-next-line: variable-name
   public carrousel_img = 0;
   public carrousel = 0;
-  public changeTab = 1;
+  public step = 0;
   public title = 'nellmaxApp';
   public images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
 
 
+  ngOnInit(){
+    setInterval(() => {
+      this.changeTab();
+    }, 5000);
+  }
+
+
+  changeTab(){
+    if(this.step == 2){
+      setTimeout(() => {
+        this.step = 0
+      }, 3000);
+    }else{
+      this.step = this.step + 1;
+    }
+  
+  }
 
 
 }
