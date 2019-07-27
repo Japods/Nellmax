8bfbc4eb250698464bb303a256797d687514b620
Aliatore <luisisturiz3@gmail.com>
2019-07-27 01:25:42 -0400

    arreglos menores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,23 +10,23 @@ export class AppComponent {
 
   // tslint:disable-next-line: variable-name
   public range = 0;
-  public sendCotization: boolean = false;
-  public sendEmail: boolean = false;
-  public showBasic: boolean = false;
+  public sendCotization = false;
+  public sendEmail = false;
+  public showBasic = false;
   public carrousel_img = 0;
   public carrousel = 0;
   public step = 0;
   public title = 'nellmaxApp';
   public images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
 
-  public service_id = 'gmail'
-  public template_id = 'template_SIL7WGGs'
-  public user_id = 'user_CzQfsRMkf4K1YmlWWcMni'
+  public service_id = 'gmail';
+  public template_id = 'template_SIL7WGGs';
+  public user_id = 'user_CzQfsRMkf4K1YmlWWcMni';
   public template_params = {
     name: '',
     reply_email: '',
     message: ''
-  }
+  };
 
   public name: any;
   public reply_email: any;
@@ -46,14 +46,14 @@ export class AppComponent {
   changeTab() {
     if (this.step == 2) {
       setTimeout(() => {
-        this.step = 0
+        this.step = 0;
       }, 3000);
     } else {
       this.step = this.step + 1;
     }
     if (this.carrousel_img == 2) {
       setTimeout(() => {
-        this.carrousel_img = 0
+        this.carrousel_img = 0;
       }, 3000);
     } else {
       this.carrousel_img = this.carrousel_img + 1;
@@ -104,7 +104,7 @@ export class AppComponent {
         }
         this.sendCotization = !this.sendCotization;
 
-        console.log("Send",this.sendCotization)
+        console.log('Send', this.sendCotization);
 
         break;
 
@@ -118,7 +118,7 @@ export class AppComponent {
 
 
   sendForm() {
-    this.template_params.name = this.name
+    this.template_params.name = this.name;
     this.template_params.reply_email = this.reply_email;
     this.template_params.message = this.message;
     console.log('This template params', this.template_params);
@@ -127,10 +127,4 @@ export class AppComponent {
       console.log('Response', response);
     });
   }
-
-  
-
-
 }
-
-
